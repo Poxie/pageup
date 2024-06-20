8e3146a00c418523ab20ba2697950def49268c3c
Poxie <poxen12@hotmail.com>
2024-06-20 15:09:52 +0200

    Add onClick prop to button

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -1,8 +1,12 @@
-export default function Button({ children }: {
+export default function Button({ children, onClick }: {
     children: React.ReactNode;
+    onClick?: () => void;
 }) {
     return (
-        <button className="px-4 py-3 bg-c-primary text-light rounded-md">
+        <button 
+            className="px-4 py-3 bg-c-primary text-light rounded-md"
+            onClick={onClick}
+        >
             {children}
         </button>
     )
