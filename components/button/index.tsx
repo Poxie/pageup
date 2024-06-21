b9030e78bb06f127d984e9674327798783de1f52
Poxie <poxen12@hotmail.com>
2024-06-22 01:29:32 +0200

    Add new button props

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -1,10 +1,17 @@
-export default function Button({ children, onClick }: {
+import { twMerge } from "tailwind-merge";
+
+export default function Button({ children, onClick, className, disabled }: {
     children: React.ReactNode;
     onClick?: () => void;
+    className?: string;
+    disabled?: boolean
 }) {
     return (
         <button 
-            className="px-4 py-3 bg-c-primary text-light rounded-md"
+            className={twMerge(
+                "px-4 py-3 bg-c-primary text-light rounded-md",
+                className,
+            )}
             onClick={onClick}
         >
             {children}
