449ebb72a4a5c26342e95d54744590801b52701c
Poxie <poxen12@hotmail.com>
2024-06-22 17:11:42 +0200

    Responsive focus section

--- components/home/HomeFocus.tsx
+++ components/home/HomeFocus.tsx
@@ -6,8 +6,8 @@ import HomeFocusCard from './HomeFocusCard';
 export default function HomeFocus() {
     return(
         <section>
-            <div className="p-section w-main max-w-main mx-auto flex divide-x-2 divide-secondary">
-                <div className="pr-8 flex-1 flex flex-col gap-3 sticky top-[20%] self-start">
+            <div className="p-section w-main max-w-main mx-auto md:flex divide-y-2 md:divide-y-0 md:divide-x-2 divide-secondary">
+                <div className="pb-5 md:pb-0 md:pr-8 flex-1 flex flex-col gap-3 md:sticky md:top-[20%] md:self-start">
                     <h2 className="text-4xl font-semibold">
                         Vårt fokus är
                         {' '}
@@ -19,7 +19,7 @@ export default function HomeFocus() {
                         Vi strävar efter att leverera webbsidor som är optimerade för alla användare, med hög tillgänglighet, SEO-optimering, responsiv design och snabb prestanda. Varje detalj är noggrant utformad för att säkerställa en användarvänlig och effektiv upplevelse oavsett enhet eller plattform
                     </span>
                 </div>
-                <ul className="pl-8 flex-1 grid grid-cols-2 gap-3">
+                <ul className="pt-5 md:pt-0 md:pl-8 flex-1 grid gap-3 lg:grid-cols-2">
                     {FocusCards.map(card => (
                         <li key={card.title}>
                             <HomeFocusCard 
