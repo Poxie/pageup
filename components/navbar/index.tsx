445a2670d0fb99b5637ee940039f284f1ae2f54f
Poxie <poxen12@hotmail.com>
2024-06-22 17:07:18 +0200

    Make navbar responsive

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,6 +1,10 @@
 "use client";
 import Link from "next/link";
 import Button from "../button";
+import { twMerge } from "tailwind-merge";
+import HamIcon from "@/assets/icons/HamIcon";
+import { useEffect, useState } from "react";
+import useScreenSize from "@/hooks/useScreenSize";
 
 const tabs = [
     { text: 'Om oss', id: 'about-us' },
@@ -8,31 +12,55 @@ const tabs = [
     { text: 'Referenser', id: 'references' },
 ]
 export default function Navbar() {
+    const screenSize = useScreenSize();
+    const isSmall = ['sm', 'md'].includes(screenSize);
+
+    const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        if(!isSmall) setOpen(false);
+    }, [isSmall]);
+
     return(
-        <nav className="w-main max-w-main p-6 absolute left-2/4 -translate-x-2/4 z-20 flex items-center justify-between">
-            <div className="flex items-center gap-8">
+        <nav className="p-6 absolute w-full left-0 z-30 flex items-center justify-between">
+            <div className="w-main max-w-main mx-auto h-[48px] flex items-center justify-between gap-8">
                 <Link
-                    className="text-2xl font-bold text-light" 
+                    className="relative z-30 text-2xl font-bold text-light" 
                     href={'/'}
                 >
                     {process.env.NEXT_PUBLIC_WEBSITE_NAME}
                 </Link>
-                <ul className="flex gap-4">
-                    {tabs.map(tab => (
-                        <li key={tab.id}>
-                            <a
-                                className="text-light"
-                                href={`#${tab.id}`}
-                            >
-                                {tab.text}
-                            </a>
-                        </li>
-                    ))}
-                </ul>
+                <div className={twMerge(
+                    "flex items-center flex-1 justify-between",
+                    isSmall && "pt-28 fixed left-0 top-0 justify-start flex-col gap-10 w-full h-[100dvh] bg-black/90 transition-[left,opacity] duration-300 ease-in-out",
+                    !open && isSmall && 'left-full opacity-0',
+                )}>
+                    <ul className="flex flex-col md:flex-row items-center gap-10 md:gap-4">
+                        {tabs.map(tab => (
+                            <li key={tab.id}>
+                                <a
+                                    className="text-3xl md:text-base text-light font-semibold md:font-normal"
+                                    href={`#${tab.id}`}
+                                >
+                                    {tab.text}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                    <Button className="text-xl font-semibold md:text-base md:font-medium">
+                        Kontakta oss
+                    </Button>
+                </div>
+                {isSmall && (
+                    <button 
+                        className={"relative z-40 text-light"}
+                        onClick={() => setOpen(!open)}
+                        aria-label={open ? 'Stäng menyn' : 'Öppna menyn'}
+                    >
+                        <HamIcon className="w-8" />
+                    </button>
+                )}
             </div>
-            <Button>
-                Kontakta oss
-            </Button>
         </nav>
     )
 }
---
+++ hooks/useScreenSize.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+type ScreenSize = 'sm' | 'md' | 'lg' | 'xl';
+
+const getScreenSize = () => {
+    if(typeof window === 'undefined') return 'xl';
+
+    const windowSize = window.innerWidth;
+
+    if (windowSize < 640) {
+        return 'sm';
+    } else if (windowSize < 768) {
+        return 'md';
+    } else if (windowSize < 1024) {
+        return 'lg';
+    } else {
+        return 'xl';
+    }
+}
+
+export default function useScreenSize() {
+    const [screenSize, setScreenSize] = useState<ScreenSize>(getScreenSize());
+  
+    useEffect(() => {
+        const handleResize = () => {
+            setScreenSize(getScreenSize());
+        };
+    
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+  
+    return screenSize;
+}
