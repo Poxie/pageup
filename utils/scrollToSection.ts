69f9e95291fdd1338bf99a80f402f68379a5831d
Poxie <poxen12@hotmail.com>
2024-06-22 18:00:41 +0200

    Different scroll offset on smaller devices

--- utils/scrollToSection.ts
+++ utils/scrollToSection.ts
@@ -1,9 +1,13 @@
 const OFFSET = 100;
+const SMALL_OFFSET = 30;
 export default function scrollToSection(sectionId: string) {
+    const windowSize = window.innerWidth;
+    const isSmallScreen = windowSize < 768;
+
     const section = document.getElementById(sectionId);
     if(section) {
         window.scrollTo({
-            top: section.offsetTop - OFFSET,
+            top: section.offsetTop - (isSmallScreen ? SMALL_OFFSET : OFFSET),
             behavior: 'smooth',
         })
     }
