da300b152229cf274f0f046f8e9f8bd84b194f38
Poxie <poxen12@hotmail.com>
2024-06-24 15:22:28 +0200

    Add scroll animation to focus section

--- components/home/HomeFocus.tsx
+++ components/home/HomeFocus.tsx
@@ -1,28 +1,44 @@
+"use client";
 import FocusCards from '@/assets/data/focus.json';
-import Image from "next/image";
-import BallDivider from "../BallDivider";
 import HomeFocusCard from './HomeFocusCard';
+import { useRef } from 'react';
+import useAnimateIntoView, { DEFAULT_INITIAL_STATE } from '@/hooks/useAnimateIntoView';
 
 export default function HomeFocus() {
+    const header = useRef<HTMLHeadingElement>(null);
+    const paragraph = useRef<HTMLParagraphElement>(null);
+
+    useAnimateIntoView(header);
+    useAnimateIntoView(paragraph, { siblingRef: header, delay: 100 });
+
     return(
         <section id="how-we-work">
             <div className="p-section w-main max-w-main mx-auto md:flex divide-y-2 md:divide-y-0 md:divide-x-2 divide-secondary">
                 <div className="pb-5 md:pb-0 md:pr-8 flex-1 flex flex-col gap-3 md:sticky md:top-[20%] md:self-start">
-                    <h2 className="text-3xl md:text-4xl font-semibold">
+                    <h2 
+                        className="text-3xl md:text-4xl font-semibold"
+                        style={DEFAULT_INITIAL_STATE}
+                        ref={header}
+                    >
                         Vårt fokus är
                         {' '}
                         <span className="text-c-primary">
                             prestation.
                         </span>
                     </h2>
-                    <span className="text-sm md:text-base">
+                    <p 
+                        className="text-sm md:text-base"
+                        style={DEFAULT_INITIAL_STATE}
+                        ref={paragraph}
+                    >
                         Vi strävar efter att leverera webbsidor som är optimerade för alla användare, med hög tillgänglighet, SEO-optimering, responsiv design och snabb prestanda. Varje detalj är noggrant utformad för att säkerställa en användarvänlig och effektiv upplevelse oavsett enhet eller plattform
-                    </span>
+                    </p>
                 </div>
                 <ul className="pt-5 md:pt-0 md:pl-8 flex-1 grid gap-3 lg:grid-cols-2">
-                    {FocusCards.map(card => (
+                    {FocusCards.map((card, index) => (
                         <li key={card.title}>
                             <HomeFocusCard 
+                                index={index}
                                 card={card}
                             />
                         </li>
--- components/home/HomeFocusCard.tsx
+++ components/home/HomeFocusCard.tsx
@@ -1,11 +1,22 @@
 import { FocusCard } from "@/assets/data/types"
 import CheckmarkIcon from "@/assets/icons/CheckmarkIcon";
+import useAnimateIntoView, { DEFAULT_INITIAL_STATE } from "@/hooks/useAnimateIntoView";
+import { useRef } from "react";
 
-export default function HomeFocusCard({ card: { title, description } }: {
+export default function HomeFocusCard({ index, card: { title, description } }: {
     card: FocusCard;
+    index: number;
 }) {
+    const ref = useRef<HTMLDivElement>(null);
+
+    useAnimateIntoView(ref);
+
     return(
-        <div className="p-5 grid gap-1 relative border-[1px] border-tertiary rounded-md">
+        <div 
+            className="p-5 h-full flex flex-col gap-1 relative border-[1px] border-tertiary rounded-md"
+            style={DEFAULT_INITIAL_STATE}
+            ref={ref}
+        >
             <div className="flex justify-between">
                 <span className="text-lg font-medium">
                     {title}
