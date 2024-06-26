c7574ea160b39e00d7db970f6440e2d092401d62
Poxie <poxen12@hotmail.com>
2024-06-26 04:54:49 +0200

    Add section main width to projects section

--- components/home/HomeProjects.tsx
+++ components/home/HomeProjects.tsx
@@ -1,12 +1,14 @@
 export default function HomeProjects() {
     return(
         <section className="p-section bg-secondary" id="references">
-            <h2 className="text-3xl md:text-4xl font-bold text-center">
-                Vi har arbetat med dessa företag.
-            </h2>
-            <p className="mt-5 text-lg text-muted text-center">
-                Den här delen kommer uppdateras snart.
-            </p>
+            <div className="w-main max-w-main mx-auto">
+                <h2 className="text-3xl md:text-4xl font-bold text-center">
+                    Vi har arbetat med dessa företag.
+                </h2>
+                <p className="mt-5 text-lg text-muted text-center">
+                    Den här delen kommer uppdateras snart.
+                </p>
+            </div>
         </section>
     )
 }
