f5fb2ac42af1d8a27249c489d2ecd4610be9a9b0
Poxie <poxen12@hotmail.com>
2024-06-26 04:28:21 +0200

    Add animation to process text & icon

--- components/home/HomeProcessCard.tsx
+++ components/home/HomeProcessCard.tsx
@@ -1,9 +1,10 @@
 import { ProcessCard } from "@/assets/data/types"
 import CloudIllustration from "@/assets/illustrations/CloudIllustration";
 import { twMerge } from "tailwind-merge";
-import React, { RefObject } from "react";
+import React, { RefObject, useRef } from "react";
 import BrainstormIcon from "@/assets/icons/BrainstormIcon";
 import HomeProcessIcon from "./HomeProcessIcon";
+import useAnimateIntoView from "@/hooks/useAnimateIntoView";
 
 type Props = {
     card: ProcessCard;
@@ -14,6 +15,12 @@ const HomeProcessCard = React.forwardRef<HTMLDivElement, Props>(({
     index,
     card: { title, description, icon }, 
 }, ref) => {
+    const textRef = useRef<HTMLDivElement>(null);
+    const iconRef = useRef<HTMLDivElement>(null);
+
+    const { initialState } =  useAnimateIntoView(textRef, { siblingRef: iconRef, threshold: .8 });
+    useAnimateIntoView(iconRef, { delay: 150, siblingRef: textRef, threshold: .8 });
+
     const reversed = index % 2 !== 0;
 
     return(
@@ -21,7 +28,11 @@ const HomeProcessCard = React.forwardRef<HTMLDivElement, Props>(({
             "flex items-center relative z-10",
             reversed ? "flex-row-reverse" : "flex-row",
         )}>
-            <div className="flex-1 relative">
+            <div 
+                className="flex-1 relative"
+                style={initialState}
+                ref={textRef}
+            >
                 <h2 className="mb-3 text-2xl md:text-3xl font-semibold">{title}</h2>
                 <p className="md:text-lg leading-8">{description}</p>
                 <div 
@@ -33,10 +44,14 @@ const HomeProcessCard = React.forwardRef<HTMLDivElement, Props>(({
                     <CloudIllustration index={index} />
                 </div>
             </div>
-            <div className={twMerge(
-                "flex-1 hidden md:flex",
-                !reversed && 'justify-end',
-            )}>
+            <div 
+                className={twMerge(
+                    "flex-1 hidden md:flex",
+                    !reversed && 'justify-end',
+                )}
+                style={initialState}
+                ref={iconRef}
+            >
                 <HomeProcessIcon 
                     icon={icon}
                 />
